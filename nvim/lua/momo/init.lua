a826a4eec90fbe7b76edb65304cbd451c3ad2b68
Jack Fraser <jackzfras@gmail.com>
2023-02-24 15:18:40 -0700

    clipboard changes

--- nvim/lua/momo/init.lua
+++ nvim/lua/momo/init.lua
@@ -2,3 +2,16 @@ require('momo.set')
 require('momo.packer')
 require('momo.remap')
 
+local has = function(x)
+  return vim.fn.has(x) == 1
+end
+
+local is_mac = has("macunix")
+local is_win = has("win32")
+
+if is_mac then 
+  require("momo.macos")
+end
+if is_win then
+  require("momo.windows")
+end
