5dc74747a4cfed470ecc77713685ffa925915265
Jack Fraser <jackzfras@gmail.com>
2023-03-22 12:31:38 -0600

    rust formatting, keybind

--- nvim/after/plugin/null-ls.lua
+++ nvim/after/plugin/null-ls.lua
@@ -2,7 +2,8 @@ local null_ls = require('null-ls')
 
 local my_sources = {
   null_ls.builtins.formatting.prettierd,
-  null_ls.builtins.formatting.black
+  null_ls.builtins.formatting.black,
+  null_ls.builtins.formatting.rustfmt
 }
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
